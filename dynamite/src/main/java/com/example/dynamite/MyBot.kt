744060dca53ece5a6c8804c9f2e3d8a4882cca25
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-08 15:38:27 +0100

    more erratic

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -9,13 +9,17 @@ class MyBot : Bot {
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-        if (dynamiteLeft(gamestate)>0 && pointsThisRound(gamestate)>=2){
+        if (dynamiteLeft(gamestate)>0 && pointsThisRound(gamestate)>= Random.nextInt(1,4)){
             return Move.D
         }
         return arrayOf(Move.R,Move.P,Move.S)[Random.nextInt(3)]
     }
     fun dynamiteLeft(gamestate: Gamestate):Int{
         return 100-gamestate.rounds.map { if (it.p1==Move.D) 1 else 0 }.sum()
+    }
+    fun gotCountered(gamestate: Gamestate):Boolean{
+        //the opponent countered our dynamite play!
+        return gamestate.rounds.any { it.p1==Move.D && it.p2==Move.W }
     }
     fun pointsThisRound(gamestate: Gamestate):Int{
         var points=0
