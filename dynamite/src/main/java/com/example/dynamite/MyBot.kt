df15a706dfe4a51ecb41e67e89a21f1812d428cd
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-09 09:46:11 +0100

    formatting in an auto fashion

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -6,45 +6,53 @@ import com.softwire.dynamite.game.Move
 import kotlin.random.Random
 
 class MyBot : Bot {
-    val rpsMoves= listOf(Move.R,Move.P,Move.S)
-    val r_cache= mutableMapOf<Pair<Int,Int>,List<Int>>()
+    val rpsMoves = listOf(Move.R, Move.P, Move.S)
+    val r_cache =
+        mutableMapOf<Pair<Int, Int>, List<Int>>() //so we don't make a new list every ranInt call
+
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-        if (dynamiteLeft(gamestate)>0 && pointsThisRound(gamestate)>= ranInt(1,4)){
+        if (dynamiteLeft(gamestate) > 0 && pointsThisRound(gamestate) >= ranInt(1, 4)) {
             return Move.D
         }
         return rpsMoves.shuffled().first()
     }
-    fun dynamiteLeft(gamestate: Gamestate):Int{
-        return 100-gamestate.rounds.map { if (it.p1==Move.D) 1 else 0 }.sum()
+
+    fun dynamiteLeft(gamestate: Gamestate): Int {
+        return 100 - gamestate.rounds.map { if (it.p1 == Move.D) 1 else 0 }.sum()
     }
-    fun gotCountered(gamestate: Gamestate):Boolean{
+
+    fun gotCountered(gamestate: Gamestate): Boolean {
         //the opponent countered our dynamite play!
-        return gamestate.rounds.any { it.p1==Move.D && it.p2==Move.W }
+        return gamestate.rounds.any { it.p1 == Move.D && it.p2 == Move.W }
     }
-    fun pointsThisRound(gamestate: Gamestate):Int{
-        var points=0
-        for (round in gamestate.rounds.asReversed()){
-            if (round.p1==round.p2){
+
+    fun pointsThisRound(gamestate: Gamestate): Int {
+        var points = 0
+        for (round in gamestate.rounds.asReversed()) {
+            if (round.p1 == round.p2) {
                 points++
-            }else{
+            } else {
                 break
             }
         }
         return points
     }
-    fun ranInt(to:Int):Int{
-        return ranInt(0,to)
+
+    fun ranInt(to: Int): Int {
+        return ranInt(0, to)
     }
-    fun ranInt(from:Int,to:Int):Int{
+
+    fun ranInt(from: Int, to: Int): Int {
         //because it be broken
-        if (r_cache.containsKey(Pair(from,to))){
-            return r_cache[Pair(from,to)]?.shuffled()?.first()?:from
+        if (r_cache.containsKey(Pair(from, to))) {
+            return r_cache[Pair(from, to)]?.shuffled()?.first() ?: from
         }
-        r_cache[Pair(from,to)]=(from until to).toList()
+        r_cache[Pair(from, to)] = (from until to).toList()
         return ranInt(from, to)
     }
+
     init {
         // Are you debugging?
         // Put a breakpoint on the line below to see when we start a new match
